b9ae153d071fe73ccbddc66c2e740a2b390d35ef
Eric Lopez Clavero <eric.lopez.clavero@est.fib.upc.edu>
2017-12-26 18:26:45 +0100

    go

--- js/Image.js
+++ js/Image.js
@@ -312,7 +312,7 @@ var CredVec = [
 	new CredData("../src/images/icred/senpq.jpg","SENAIDA & PQBROS",10),
 	new CredData("../src/images/icred/pica.jpg","NOT YOU",5),
 	new CredData("../src/images/icred/yeray.jpg","WAIFULOVER",10),
-	new CredData("../src/images/icred/kano.jpg","XPEDROX",10),
+	new CredData("../src/images/icred/kano.jpg","YOUNG KANO",10),
 ]
 
 //---------------------------------------------------------------------------------------
@@ -327,15 +327,17 @@ var j = 0;
 var r = 0;
 var x = 0;
 var end = 0;
-var solr = 0;
 var interval;
 var interval2;
 var interval3;
+var interval4;
 var ImageVec = [];
 var ImageMat = [EricVec,FerVec,GrewVec,KanoVec,MarcVec,YerayVec,EliraVec,MarcPQVec,CrisPQVec,Cp2Vec];
-//var ImageMat = [Cp2Vec];
-//var ImageMat = [TestVec];
-var itot = 100;
+var ImageMat = [YerayVec]
+var itot = 17;
+var tei = 0;
+var tej = 0;
+var first = true;
 
 //---------------------------------------------------------------------------------------
 //--------------------------------- SOME FUNCTIONS --------------------------------------
@@ -385,8 +387,11 @@ var itot = 100;
 
 function next_cred() {
 	iter = iter + 1;
-	$("#Image").css("backgroundImage",'url('+CredVec[iter].link+')');
-	document.getElementById("Last").innerHTML = CredVec[iter].author
+	if(iter < CredVec.length) {
+		$("#Image").css("backgroundImage",'url('+CredVec[iter].link+')');
+		document.getElementById("Last").innerHTML = CredVec[iter].author;
+	}
+	else clearInterval(interval3)
 }
 
 function show_cred() {
@@ -394,13 +399,43 @@ function show_cred() {
 	document.getElementById("Last").innerHTML = CredVec[iter].author
 	interval3 = setInterval(function() {
 			--CredVec[iter].time;
-			if(CredVec[iter].time <= 0) {
+			if(CredVec[iter].time == -1) {
 				next_cred()
 			}
 		},1000);
 	$("#Song").attr("src", "../src/music/other/xmascred.mp3");
 	document.getElementById("TextN").innerHTML = "THANKS ALL FOR PLAYING AND SPECIAL THANKS TO ..."	
 }
+
+function next_true_ending() {
+    ImageMat[tei][tej].used = true;
+    tej = tej + 1;
+	if (tej == ImageMat[tei].length) {
+		tej = 0;
+		tei = tei + 1;
+		if (tei == ImageMat.length) {
+			$(".game").hide();
+			$("#Song").trigger("pause");
+		}
+	}
+    while(ImageMat[tei][tej].used) {
+		tej = tej + 1;
+		if (tej == ImageMat[tei].length) {
+			tej = 0;
+			tei = tei + 1;
+			if (tei == ImageMat.length) {
+				$(".game").hide();
+				$("#Song").trigger("pause");
+			}
+		}
+	}
+    show_name();
+    $("#Image").css("backgroundImage",'url('+ImageMat[tei][tej].link+')');
+    document.getElementById("demo").innerHTML = ImageMat[tei][tej].time;
+    $("#demo").show()
+    document.getElementById("Last").innerHTML = "Imagen de " + ImageMat[tei][tej].author;
+}
+
 
 function next() {
 	$("body").css("backgroundImage","url(../src/images/other/xmas.png)");
@@ -412,7 +447,6 @@ function next() {
 		while(ImageVec[x].used == true) {
 			x = Math.floor((Math.random() * ImageVec.length));
         }
-        solvec.push(ImageVec[x].sols[0]);
         show_name();
         $("#Image").css("backgroundImage",'url('+ImageVec[x].link+')');
         document.getElementById("demo").innerHTML = ImageVec[x].time;
@@ -420,7 +454,7 @@ function next() {
      }
      else {
 		 end = 1;
-		 show_cred();
+		 //show_cred();
 	 }
 }
  
@@ -443,6 +477,40 @@ function next_xmas() {
 		 r = Math.floor((Math.random() * XmasVec.length));
 		 $("#Song").attr("src", XmasVec[r].link);
 	 }
+}
+
+function true_ending() {
+	first = false;
+	document.getElementById("TextN").innerHTML = "TRUE ENDING UNLOCKED!"	
+	clearInterval(interval)
+	clearInterval(interval2)
+	while(ImageMat[tei][tej].used) {
+		tej = tej + 1;
+		if (tej == ImageMat[tei].length) {
+			tej = 0;
+			tei = tei + 1;
+			if (tei == ImageMat.length) {
+				$(".game").hide();
+				$("#Song").trigger("pause");
+			}
+		}
+	}
+	$("#Image").css("backgroundImage",'url('+ImageMat[tei][tej].link+')');
+	document.getElementById("demo").innerHTML = ImageMat[tei][tej].time;
+	$("#demo").show()
+	document.getElementById("Last").innerHTML = "Imagen de " + ImageMat[tei][tej].author;
+	interval4 = setInterval(function() {
+		--ImageMat[tei][tej].time;
+		document.getElementById("demo").innerHTML = ImageMat[tei][tej].time;
+		if(ImageMat[tei][tej].time == -1) {
+			$("#demo").hide()
+			document.getElementById("Last").innerHTML = "Solución: " + ImageMat[tei][tej].sols[0]
+		}
+		else if(ImageMat[tei][tej].time == -6) {
+			next_true_ending();
+		} 
+	},1000);
+			
 }
 
 //---------------------------------------------------------------------------------------
@@ -476,6 +544,12 @@ $(document).ready(function() {
 			}
 		},1000);
     $("body").keyup(function(event){
-        if(event.keyCode == 13) next();
+        if(event.keyCode == 13) ImageVec[x].time = 0;
+    });
+    $("body").keyup(function(event){
+        if(event.keyCode == 187) {
+			if (first) true_ending();
+			else ImageMat[tei][tej].time = 0;
+		}
     });
 });
